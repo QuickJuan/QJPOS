f69fe680d40569f4d98b22bdffc983394c764872
Joseph Salac <salacsoft22@gmail.com>
2025-10-14 11:25:39 +0800

    fixing domain central login

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,9 +17,10 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        '127.0.0.1',
-        'localhost',
         config('app.central_domain'),
+        'storepos.online',
+        'www.storepos.online',
+        'localhost',
     ],
 
     /**
